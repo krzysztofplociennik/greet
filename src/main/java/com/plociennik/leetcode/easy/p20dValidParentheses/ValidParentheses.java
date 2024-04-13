2643c9cac5276e1c5d5d069eff50dde0d3086b4e
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2024-04-13 11:53:27 +0200

    Added a 'Valid Parentheses' problem.

---
+++ src/main/java/com/plociennik/leetcode/easy/p20dValidParentheses/ValidParentheses.java
@@ -0,0 +1,26 @@
+package com.plociennik.leetcode.easy.p20dValidParentheses;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(isValid("(}"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+    }
+
+    public static boolean isValid(String s) {
+        String result = subtractValidParentheses(s);
+        return result.length() == 0;
+    }
+
+    public static String subtractValidParentheses(String s) {
+        String temp = s.replace("()", "").replace("[]", "").replace("{}", "");
+        if (s.length() == temp.length()) {
+            return temp;
+        } else {
+            return subtractValidParentheses(temp);
+        }
+    }
+}
