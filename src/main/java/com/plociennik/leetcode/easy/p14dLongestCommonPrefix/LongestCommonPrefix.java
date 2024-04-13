ed624c5985d637ad572d0a737d41c8cf70f02163
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2024-04-13 11:33:54 +0200

    Added a 'Longest Common Prefix' problem.

---
+++ src/main/java/com/plociennik/leetcode/easy/p14dLongestCommonPrefix/LongestCommonPrefix.java
@@ -0,0 +1,57 @@
+package com.plociennik.leetcode.easy.p14dLongestCommonPrefix;
+
+import java.util.Arrays;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ *
+ * Example 2:
+ *
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *     1 <= strs.length <= 200
+ *     0 <= strs[i].length <= 200
+ *     strs[i] consists of only lowercase English letters.
+ */
+
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        String result ="";
+
+        boolean canLoop = true;
+        int index = 0;
+        while (canLoop) {
+            for (int i = 0; i < strs.length - 1; i++) {
+                if (strs[0].length() < index + 1) {
+                    canLoop = false;
+                    break;
+                }
+
+            }
+        }
+
+
+        return result;
+    }
+}
---
+++ src/main/java/com/plociennik/leetcode/easy/p14dLongestCommonPrefix/LongestCommonPrefix2.java
@@ -0,0 +1,72 @@
+package com.plociennik.leetcode.easy.p14dLongestCommonPrefix;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ *
+ * Example 2:
+ *
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *     1 <= strs.length <= 200
+ *     0 <= strs[i].length <= 200
+ *     strs[i] consists of only lowercase English letters.
+ */
+
+public class LongestCommonPrefix2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(longestCommonPrefix(new String[] {"flower","flow","flight"}));
+        System.out.println(longestCommonPrefix(new String[] {"dog","racecar","car"}));
+        System.out.println(longestCommonPrefix(new String[] {"flower","flower","flower","flower"}));
+        System.out.println(longestCommonPrefix(new String[] {"ab", "a"}));
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        StringBuilder result = new StringBuilder();
+
+        if (strs.length == 0) {
+            return result.toString();
+        }
+        if (strs.length == 1) {
+            return strs[0];
+        }
+
+        String str = strs[0];
+
+        int index = 0;
+        while (index < str.length()) {
+            char c = str.charAt(index);
+
+            for (int i = 1; i < strs.length; i++) {
+                String currentString = strs[i];
+                if (currentString.length() == result.length() || currentString.charAt(index) != c) {
+                    return result.toString();
+
+                }
+            }
+            result.append(c);
+            index++;
+        }
+
+
+        return result.toString();
+    }
+}
